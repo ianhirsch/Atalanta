dbfbaa1dfc5e02f3e48f7d6f673072409b013558
nilpilihp <56451950+nilpilihp@users.noreply.github.com>
2020-03-07 21:22:44 -0600

    homepage map frag implemented

--- app/src/main/java/com/example/atalanta/MainActivity.java
+++ app/src/main/java/com/example/atalanta/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.atalanta;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.security.cert.CertificateFactory;
+
+public class MainActivity extends FragmentActivity {
+
+    //private final LatLng mDestinationLatLng = new LatLng(43.0754, 89.4042);
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Display google maps in fragment
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            displayMyLocation(); // called in getMapAsync since want map ready then display current marker
+        });
+    }
+
+    /**
+     * Handles the result of the request for location permissions
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]grantResults)
+    {
+        if(requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        {
+            // if request is cancelled, result arrays are empty
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                displayMyLocation();
+        }
+    }
+    public  void startButtonClick()
+    {
+        //Toast.makeText(this, "start button pressed",Toast.LENGTH_SHORT).show();
+    }
+    private void displayMyLocation() {
+        // Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        // if not, ask for permission
+        if(permission == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // if granted, display marker at current location
+        else
+        {
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this,task ->
+            {
+                Location mLastKnownLocation = task.getResult();
+                if(task.isSuccessful() && mLastKnownLocation != null)
+                {
+                    LatLng mLastKnownLatLng = new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude());
+                    // move camera
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(mLastKnownLatLng));
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                    // add current position marker
+                    mMap.addMarker(new MarkerOptions().position(mLastKnownLatLng));
+                }
+            });
+        }
     }
 }
+
